affbe9d430ebc086892b6295d3bc85afed54be45
Alex <2930807240@qq.com>
2022-02-24 11:28:58 +0800

    Initial commit

---
+++ src/main/java/top/alexmmd/resource/AlexmmdResourceApplication.java
@@ -0,0 +1,13 @@
+package top.alexmmd.resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlexmmdResourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlexmmdResourceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/top/alexmmd/resource/AlexmmdResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package top.alexmmd.resource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlexmmdResourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
